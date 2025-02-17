dca27ad97cdf38e7f8b25eb692da58d51dc2bdbb
Vim <36118616+ps-vivek@users.noreply.github.com>
2025-02-17 11:36:27 -0700

    Added encapsulation

--- src/oops/MineCraftBlock.java
+++ src/oops/MineCraftBlock.java
@@ -1,12 +1,36 @@
 package oops;
 
-// Class (Blueprint)
-class MinecraftBlock {
-    // Attributes (Properties)
-    String material;
-    String color;
+/*
+        The attributes material and color are now private.
+         Public getter and setter methods are provided to access and modify these attributes.
+     Encapsulation ensures that the internal state of the object is protected and can only be changed through the public methods.
+ */
 
-    MinecraftBlock(String material,String color){
+
+// ClassEncapsulation ensures that the internal state of the object is protected and can only be changed through the public methods. (Blueprint)
+class MinecraftBlock {
+    // AThis way, we have added encapsulation to your code, improving its robustness and security.ttributes (Properties)
+    private String material="Gold";
+    private String color="Yellow";
+    int weight; //This can be accessed outside
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    MinecraftBlock(String material, String color){
         this.material = material;
         this.color = color;
     }
@@ -18,6 +42,6 @@ class MinecraftBlock {
 
     // Method (Action)
     public void place() {
-        System.out.println("Placing a " + color + " " + material + " block.");
+        System.out.println("Placing a " + this.getColor() + " " + this.getMaterial() + " block.");
     }
 }
--- src/oops/MineCraftMain.java
+++ src/oops/MineCraftMain.java
@@ -1,30 +1,6 @@
 package oops;
 
-/**
- *
- * Class
- * Definition: A class is like a blueprint or a recipe.
- * It defines what attributes (properties) and
- * methods (actions) an object should have.
- *
- * Purpose: It provides a template to create objects with specific characteristics and behaviors.
- *
- * Example in Minecraft:
- *  Class: Think of a "Blueprint for a Minecraft Block".
- *  It defines the properties (like color, material) and methods (like place, destroy)
- *  that a Minecraft block should have.
- *
- * Object
- * Definition: An object is an instance of a class.
- * It is a concrete item created using the blueprint provided by the class.
- *
- * Purpose: It represents a specific item that can be used and manipulated in the program.
- *
- * Example in Minecraft:
- * Object: Think of an actual "Minecraft Block" you place in the game.
- * It is created based on the blueprint and has specific properties (like being a wooden block) and
- * can perform actions (like being placed or destroyed).
- */
+
 public class MineCraftMain {
     public static void main(String[] args) {
         // Creating objects (Instances of the class)
@@ -34,5 +10,15 @@ public class MineCraftMain {
         // Using objects
         woodenBlock.place(); // Outputs: Placing a Brown Wood block.
         stoneBlock.place(); // Outputs: Placing a Gray Stone block.
+
+        MinecraftBlock ironBlock = new MinecraftBlock();
+        ironBlock.weight = 3;
+        //Private fields cant be accessed outside
+        //ironBlock.color = "blue";
+        ironBlock.place();
+
+        ironBlock.setColor("copper");
+        ironBlock.setMaterial("Iron");
+        ironBlock.place();
     }
 }
